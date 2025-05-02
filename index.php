99df522e1fad8922806f29fe15b5012042f64689
BrunoArrudas <brunoguilhermechaveiro@gmail.com>
2025-05-02 18:33:40 -0300

    Criação das paginas index e perfil.

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+include_once 'header.php';
+?>
+
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site Dragões</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div id="container-img">
+
+    <div id="container-dragao">
+
+        <img src="Images/fundoPrincipal.jpg" alt="">
+
+    </div>
+
+</div>
+
+<div>
+    <h1>DESCUBRA SEU DRAGÃO</h1>
+</div>
+
+<div id="container-img2">
+
+    <div id="container-dragao2">
+
+        <img src="Images/fundoPrincipal2.png" alt="">
+
+    </div>
+
+</div>
+    
+</body>
+</html>
+
+<?php
+include_once 'footer.php';
+?>
