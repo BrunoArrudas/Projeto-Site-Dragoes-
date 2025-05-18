0b4d0842b444cda41522c08f5afd5078321fb50c
BrunoArrudas <brunoguilhermechaveiro@gmail.com>
2025-05-17 21:50:58 -0300

    Atualização Script SQL.

--- projeto_dragoes.sql
+++ projeto_dragoes.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE projeto_dragoes;
+CREATE DATABASE  projeto_dragoes;
 
 CREATE TABLE `info_usuarios` (
-  `id` PRIMARY KEY NOT NULL,
-  `nome` varchar(50) DEFAULT NULL,
-  `data_nascimento` varchar(30) DEFAULT NULL,
-  `email` varchar(10) DEFAULT NULL,
-  `telefone` varchar(50) DEFAULT NULL,
-  `senha` varchar(20) DEFAULT NULL
+  `id` INT AUTO_INCREMENT PRIMARY KEY NOT,
+  `nome` varchar(50) NOT NULL,
+  `data_nascimento` varchar(15) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `telefone` varchar(20) NOT NULL,
+  `senha` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
