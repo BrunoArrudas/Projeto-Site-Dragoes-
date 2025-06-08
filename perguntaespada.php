ead96186dd9c959ba6804ed60d42b8c55b2ee15d
BrunoArrudas <brunoguilhermechaveiro@gmail.com>
2025-06-08 11:53:11 -0300

    Criação da pagian perguntaespada.php e atualização da pagina elementos.

--- perguntaespada.php
+++ perguntaespada.php
@@ -2,22 +2,22 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <title>Qual seu elemento?</title>
+    <title>Qual sua espada?</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="cards.css">
 </head>
 <body>
 
-<h3 style="text-align: center;">Se você fosse um dragão, qual seria a sua conexão com os elementos da natureza?</h3>
+<h3 style="text-align: center;">Qual espada você empunharia?</h3>
 
 <div class="container">
-    <a href="perguntaespada.php?elemento=fogo" class="card">
+    <a href="pergunta.php?elemento=fogo" class="card">
         <img src="images/fogo01.jpg" alt="Fogo">
     </a>
-    <a href="perguntaespada.php?elemento=agua" class="card">
+    <a href="pergunta.php?elemento=agua" class="card">
         <img src="images/agua.jpg" alt="Água">
     </a>
-    <a href="perguntaespada.php?elemento=terra" class="card">
+    <a href="pergunta.php?elemento=terra" class="card">
         <img src="images/terra.jpg" alt="Terra">
     </a>
 </div>
@@ -26,15 +26,15 @@
 
 <div class="container">
     
-     <a href="perguntaespada.php?elemento=ar" class="card">
+     <a href="pergunta.php?elemento=ar" class="card">
         <img src="images/ar.jpg" alt="Ar">
     </a>
 
-     <a href="perguntaespada.php?elemento=gelo" class="card">
+     <a href="pergunta.php?elemento=gelo" class="card">
         <img src="images/gelo.jpg" alt="Gelo">
     </a>
 
-     <a href="perguntaespada.php?elemento=ar" class="card">
+     <a href="pergunta.php?elemento=ar" class="card">
         <img src="images/relampago.jpg" alt="Relampago">
     </a>
 </div>
