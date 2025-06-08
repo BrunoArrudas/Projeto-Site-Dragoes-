21259ce23c054e97a79ceb2bf052888ea12d9290
BrunoArrudas <brunoguilhermechaveiro@gmail.com>
2025-06-08 14:19:48 -0300

    Criação pagina perguntareino.php

---
+++ perguntareino.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Reino</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="cards.css">
+</head>
+<body>
+
+<h3 style="text-align: center;">Qual seria o seu reino?</h3>
+
+<div class="container">
+    <a href="perguntareino.php?reino=flutuante" class="card">
+        <img src="images/flutuante.jpg" alt="flutuante">
+    </a>
+    <a href="perguntareino.php?reino=castelovulcao" class="card">
+        <img src="images/castelovulcao.jpg" alt="castelovulcao">
+    </a>
+    <a href="perguntareino.php?reino=catesteloprofundezas" class="card">
+        <img src="images/casteloprofundezas.jpg" alt="casteloprofundezas">
+    </a>
+</div>
+
+<br><br>
+
+<div class="container">
+    
+     <a href="perguntareino.php?reino=florestaencantada" class="card">
+        <img src="images/florestaencantada.jpg" alt="florestaencantada">
+    </a>
+
+     <a href="perguntareino.php?reino=desertoarido" class="card">
+        <img src="images/desertoarido.jpg" alt="desertoarido">
+    </a>
+
+       <a href="perguntareino.php?reino=reinocongelado" class="card">
+        <img src="images" alt="reinocongelado">
+    </a>
+
+  
+</div>
+
+</body>
+</html>
