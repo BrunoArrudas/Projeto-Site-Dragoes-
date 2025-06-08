f4aa03b048b5987755fe012caa2993fe7a08c5b4
BrunoArrudas <brunoguilhermechaveiro@gmail.com>
2025-06-08 12:58:47 -0300

    Atualização pagina perguntacor.php

--- perguntaguardiao.php
+++ perguntaguardiao.php
@@ -2,22 +2,22 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <title>Cor</title>
+    <title>Guardião</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="cards.css">
 </head>
 <body>
 
-<h3 style="text-align: center;">Se você fosse um dragão, qual seria a sua cor?</h3>
+<h3 style="text-align: center;">Qual guardião protegeria o seu reino?</h3>
 
 <div class="container">
-    <a href="perguntaguardiao.php?cor=vermelho" class="card">
+    <a href="perguntaguardiao.php?guardiao=vermelho" class="card">
         <img src="images/vermelho.jpg" alt="vermelho">
     </a>
-    <a href="perguntaguardiao.php?cor=azul" class="card">
+    <a href="perguntaguardiao.php?guardiao=azul" class="card">
         <img src="images/azul.jpg" alt="azul">
     </a>
-    <a href="perguntaguardiao.php?cor=verde" class="card">
+    <a href="perguntaguardiao.php?guardiao=verde" class="card">
         <img src="images/verde.jpg" alt="verde">
     </a>
 </div>
@@ -26,15 +26,15 @@
 
 <div class="container">
     
-     <a href="perguntaguardiao.php?cor=dourado" class="card">
+     <a href="perguntaguardiao.php?guardiao=dourado" class="card">
         <img src="images/dourado.jpg" alt="dourado">
     </a>
 
-     <a href="perguntaguardiao.php?cor=preto" class="card">
+     <a href="perguntaguardiao.php?guardiao=preto" class="card">
         <img src="images/preto.png" alt="preto">
     </a>
 
-     <a href="perguntaguardiao.php?cor=prateado" class="card">
+     <a href="perguntaguardiao.php?guardiao=prateado" class="card">
         <img src="images/prateado.png" alt="prateado">
     </a>
 </div>
