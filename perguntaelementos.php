084ccd760b9242eb6add7910b20682d52dad1581
BrunoArrudas <brunoguilhermechaveiro@gmail.com>
2025-06-07 22:33:58 -0300

    criação da pagina perguntaelementos.php

---
+++ perguntaelementos.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Qual seu elemento?</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="cards.css">
+</head>
+<body>
+
+<h3 style="text-align: center;">Se você fosse um dragão, qual seria a sua conexão com os elementos da natureza?</h3>
+
+<div class="container">
+    <a href="pergunta.php?elemento=fogo" class="card">
+        <img src="images/fogo01.jpg" alt="Fogo">
+    </a>
+    <a href="pergunta.php?elemento=agua" class="card">
+        <img src="images/agua.jpg" alt="Água">
+    </a>
+    <a href="pergunta.php?elemento=terra" class="card">
+        <img src="images/terra.jpg" alt="Terra">
+    </a>
+</div>
+
+<br><br>
+
+<div class="container">
+    
+     <a href="pergunta.php?elemento=ar" class="card">
+        <img src="images/ar.jpg" alt="Ar">
+    </a>
+
+     <a href="pergunta.php?elemento=gelo" class="card">
+        <img src="images/gelo.jpg" alt="Gelo">
+    </a>
+
+     <a href="pergunta.php?elemento=ar" class="card">
+        <img src="images/relampago.jpg" alt="Relampago">
+    </a>
+</div>
+
+</body>
+</html>
